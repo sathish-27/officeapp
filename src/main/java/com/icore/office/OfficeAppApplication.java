d83f81a339e029b1e23311154e2052da1620e576
s.sathishkumar <s.sathishkumar.cse27@gmail.com>
2018-04-19 22:27:44 +0530

    initial commit

---
+++ src/main/java/com/icore/office/OfficeAppApplication.java
@@ -0,0 +1,12 @@
+package com.icore.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/icore/office/OfficeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.icore.office;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OfficeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
